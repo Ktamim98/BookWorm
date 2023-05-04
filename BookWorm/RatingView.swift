9efba13167a5dacb4715bc6d10d91137664f0f62
Ktamim98 <tamim15-2515@diu.edu.bd>
2023-05-04 18:18:55 +0600

    . voiceOver fixed

--- BookWorm/RatingView.swift
+++ BookWorm/RatingView.swift
@@ -37,6 +37,22 @@ struct RatingView: View {
                 
             }
         }
+        .accessibilityElement()
+        .accessibilityLabel(label)
+        .accessibilityValue(rating == 1 ? "1 star" : "\(rating) star")
+        .accessibilityAdjustableAction{ direction in
+            switch direction{
+            case .increment:
+                if rating < maxRating {rating += 1}
+            case .decrement:
+                if rating > 1 {rating -= 1}
+            default:
+                break
+            }
+            
+        }
+        
+        
     }
     
     func image(for number: Int) -> Image{
